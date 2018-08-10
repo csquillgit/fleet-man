d4b81a72666b567b6f0c036d6c65c7d6cb094dcf
CSquill <CSquill@CSQUILL-M7520.dcli.local>
2018-08-10 07:36:51 -0400

    refactor

--- app/src/main/java/com/fleetman/FleetManController.java
+++ app/src/main/java/com/fleetman/FleetManController.java
@@ -2,11 +2,17 @@ package com.fleetman;
 
 import java.util.logging.Logger;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.core.CoreService;
+
 @RestController
 public class FleetManController {
+
+	@Autowired
+	CoreService cs;
 
 	protected Logger logger = Logger.getLogger(FleetManController.class.getName());
 
--- facade1/src/main/java/com/facade1/Facade1Application.java
+++ facade1/src/main/java/com/facade1/Facade1Application.java
@@ -2,8 +2,10 @@ package com.facade1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.core", "com.facade1" })
 public class Facade1Application {
 
 	public static void main(String[] args) {
